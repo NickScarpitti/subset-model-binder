057b82ca05b683eca8098a6db9e805a890f35d16
Nick Scarpitti <nick_scarpitti@na.honda.com>
2024-08-30 16:39:43 -0400

    Added allowInheritedProperties to SubsetOfAttribute

--- subset-model-binder/SubsetOfAttribute.cs
+++ subset-model-binder/SubsetOfAttribute.cs
@@ -1,8 +1,9 @@
 ﻿namespace Subset_Model_Binder;
 
 [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-public class SubsetOfAttribute(Type sourceType, bool isMvcApp = false) : Attribute
+public class SubsetOfAttribute(Type sourceType, bool isMvcApp = false, bool allowInheritedProperties = true) : Attribute
 {
     public Type SourceType { get; } = sourceType;
     public bool IsMvcApp { get; } = isMvcApp;
+    public bool AllowInheritedProperties { get; } = allowInheritedProperties;
 }
