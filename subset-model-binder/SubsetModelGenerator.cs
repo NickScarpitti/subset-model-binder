a844e3d55ed1655f59488fadfb3114f9806d0a7d
Nick Scarpitti <nick_scarpitti@na.honda.com>
2024-08-30 09:39:11 -0400

    Moved SubsetOfAttribute to its own class Updated package info

--- subset-model-binder/SubsetModelGenerator.cs
+++ subset-model-binder/SubsetModelGenerator.cs
@@ -8,13 +8,6 @@ using Microsoft.CodeAnalysis.Text;
 
 namespace Subset_Model_Binder;
 
-[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-public class SubsetOfAttribute(Type sourceType, bool isMvcApp = false) : Attribute
-{
-    public Type SourceType { get; } = sourceType;
-    public bool IsMvcApp { get; } = isMvcApp;
-}
-
 [Generator]
 public class SubsetValidatorGenerator : IIncrementalGenerator
 {
---
+++ subset-model-binder/SubsetOfAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace Subset_Model_Binder;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public class SubsetOfAttribute(Type sourceType, bool isMvcApp = false) : Attribute
+{
+    public Type SourceType { get; } = sourceType;
+    public bool IsMvcApp { get; } = isMvcApp;
+}
